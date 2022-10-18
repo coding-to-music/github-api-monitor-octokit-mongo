7adccab1632b019eb87c0039c996180a0c5ecdb5
coding-to-music <connors.tom@gmail.com>
2022-10-18 21:33:00 +0000

    Build

--- startup/db.js
+++ startup/db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-let db = "mongodb://localhost:27017/loggedPullRequests";
+let db = process.env.MONGO_URI;
 module.exports = function () {
   mongoose.connect(db).then(() => console.log(`Connected to ${db}...`));
 };
